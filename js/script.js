49774b942af8c48df6e1d61c8a585fc10ce9e698
Eduardo Souza <103686965+DevEduardoSouza@users.noreply.github.com>
2023-08-05 08:55:00 -0300

    Adicionar paginação no home

--- js/script.js
+++ js/script.js
@@ -264,9 +264,9 @@ const createNews = (news) => {
 
 // Preencher as ùltimas notícias
 
-animeNewsData.map((news)=>{
-    createNews(news);
-  });
+// animeNewsData.map((news)=>{
+//     createNews(news);
+//   });
 
   /*
 allNews.then(data => {
@@ -278,7 +278,6 @@ allNews.then(data => {
     console.error('Error fetching all news:', error);
 });
 */
-
 
 // Criar estrutura das news highlight
 const createHighLightNews = (news) =>{
@@ -302,11 +301,9 @@ const createHighLightNews = (news) =>{
 
     listNewsHighlight.appendChild(divNewsHighligh);
 }
-
 animeNewsData.map((news)=>{
     createHighLightNews(news);
 })
-
 
 // 
 btnNext.addEventListener('click', ()=>{
@@ -341,8 +338,6 @@ const upadateNewsCarrousel = () => {
 }
 upadateNewsCarrousel();
 
-
-
 //---------------------{ Eventos }---------------------
 
 setInterval(()=>{
@@ -354,5 +349,148 @@ setInterval(()=>{
     upadateNewsCarrousel();
 }, 5000);
 
-//---------------------{ Painel Administrativo }---------------------
+
+//---------------------{ Paginação }---------------------
+let perPage = 10;
+const state = {
+    page: 1,
+    perPage,
+    totalPage: Math.ceil(animeNewsData.length / perPage),
+    maxVisibleButtons: 5
+}
+
+const html = {
+  get(element) {
+    return document.querySelector(element);
+  }
+}
+
+const controls = {
+  next() {
+    state.page++;
+
+    if(state.page > state.totalPage){
+      state.page--;
+    }
+  },
+  prev() {
+    state.page--;
+
+    if(state.page < 1){
+      state.page++;
+    }
+  },
+  goTo(page) {
+    if(page < 1){
+      page = 1;
+    }
+
+    state.page = +page;
+
+    if(page > state.totalPage){
+      state.page = state.totalPage;
+    }
+  },
+  createListeners() {
+    html.get('.first').addEventListener('click', ()=>{
+      controls.goTo(1);
+      update();
+    });
+
+    html.get('.last').addEventListener('click', ()=>{
+      controls.goTo(state.totalPage);
+      update();
+    })
+
+    html.get('.next').addEventListener('click', ()=>{
+      controls.next();
+      update();
+    });
+
+    html.get('.prev').addEventListener('click', ()=>{
+      controls.prev();
+      update();
+    })
+
+  }
+}
+
+const buttons = {
+  create(number) {
+    const button = document.createElement('div');
+
+    button.innerHTML = number;
+
+    if(state.page ==  number){
+      button.classList.add('active');
+    }
+
+    button.addEventListener('click', (event)=>{
+      const page = event.target.innerText;
+      controls.goTo(page);
+      update();
+    })
+
+    html.get('.numbers').appendChild(button);
+  },
+  update() {
+    html.get('.numbers').innerHTML = '';
+
+    const {maxLeft, maxRight} = buttons.calculateMaxVisible();
+
+    for(let page = maxLeft; page <= maxRight; page++){
+      buttons.create(page);
+    }
+
+  },
+  calculateMaxVisible() {
+    const {maxVisibleButtons} =  state;
+    let maxLeft = (state.page - Math.floor( maxVisibleButtons /2));  
+    let maxRight = (state.page + Math.floor(maxVisibleButtons/2));
+
+    if(maxLeft < 1) {
+      maxLeft = 1;
+      maxRight = maxVisibleButtons;
+    }
+
+    if(maxRight > state.totalPage) {
+      maxLeft = state.totalPage - (maxVisibleButtons - 1)
+      maxRight = state.totalPage;
+
+      if (maxLeft<1) maxLeft = 1;
+    }
+
+    return { maxLeft , maxRight};
+  }
+}
+
+const list = {
+  create(item) {
+    createNews(item);
+  },
+  update() {
+    html.get('.content_latest_news').innerHTML = '';
+
+    let page = state.page - 1;
+    let start = page * state.perPage;
+    let end = start + state.perPage;
+
+    const paginatedItems = animeNewsData.slice(start, end);
+    console.log(paginatedItems);
+    
+    paginatedItems.forEach(list.create);
+  },
+}
+
+function update() {
+  list.update();
+  buttons.update();
+}
+
+function init() {
+  update();
+  controls.createListeners();
+}
+
+init();
 
